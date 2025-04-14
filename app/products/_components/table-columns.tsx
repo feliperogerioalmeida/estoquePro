63a9ae7101c8ecb28175b6d17f495a8a2126ab50
Felipe Almeida <felipe.rogerio@outlook.com>
2025-04-14 20:44:02 -0300

    feat: add products table

---
+++ app/products/_components/table-columns.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { Badge } from "@/app/_components/ui/badge";
+import { Product } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { CircleIcon } from "lucide-react";
+
+const getStatusLabel = (status: string) => {
+  if (status == "IN_STOCK") {
+    return "Em estoque";
+  }
+  return "Fora de Estoque";
+};
+
+export const productTableColumns: ColumnDef<Product>[] = [
+  {
+    accessorKey: "name",
+    header: "Produto",
+  },
+  {
+    accessorKey: "price",
+    header: "Valor unitário",
+  },
+  {
+    accessorKey: "stock",
+    header: "Estoque",
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: (row) => {
+      const product = row.row.original;
+      const label = getStatusLabel(product.status);
+      return (
+        <Badge
+          variant={label == "Em estoque" ? "default" : "outline"}
+          className="gap-1.5"
+        >
+          <CircleIcon
+            className={`${label == "Em estoque" ? "fill-primary-foreground" : "fill-destructive"}`}
+            size={14}
+          />
+          {label}
+        </Badge>
+      );
+    },
+  },
+];
