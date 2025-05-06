58739b70d2424139db9fbe754b618265af4fa545
Felipe Almeida <felipe.rogerio@outlook.com>
2025-05-06 20:39:31 -0300

    refactor: add Header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import {
+  Header,
+  HeaderLeft,
+  HeaderSubtitle,
+  HeaderTitle,
+} from "./_components/header";
+
 export default function Home() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+      <Header>
+        <HeaderLeft>
+          <HeaderSubtitle>Visão Geral dos dados</HeaderSubtitle>
+          <HeaderTitle>Dashboard</HeaderTitle>
+        </HeaderLeft>
+      </Header>
+    </div>
+  );
 }
